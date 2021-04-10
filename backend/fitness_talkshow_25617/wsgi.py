839cc966ccc6904af8c9b13fe3ba21fd86e0e06a
Crowdbotics <team@crowdbotics.com>
2021-04-10 15:23:35 +0000

    Update models

--- backend/fitness_talkshow_25617/wsgi.py
+++ backend/fitness_talkshow_25617/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_talkshow_25617.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitness_talkshow_25617.settings")
 
 application = get_wsgi_application()
